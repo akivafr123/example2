14a1afba9b976031044bb8c9cde0b72674a57c16
Akiva Friedman-Rosner <arosner.akiva@gmail.com>
2016-03-27 15:57:17 -0400

    Squashed bug

--- example2/app.js
+++ example2/app.js
@@ -2,17 +2,16 @@ var main = function() {
 	"use strict";
 
 	$(".comment-input button").on("click", function(event) {
-		var $new_comment = $("<p>"),
-			comment_text = $(".comment-input input").val();
+		var $new_comment;
+		
+		if ($(".comment-input input").val() !=="") {
+			$new_comment = $("<p>").text($(".comment-input input").val());
+			$(".comments").append($new_comment);		
 
-		$new_comment.text(comment_text);
-
-		// can be chained
-		// var $new_comment = $("<p>").text("this is a new comment");
-		// or
-		// var $new_comment = $("<p>").text($("comment-input input").val());
-		$(".comments").append($new_comment);
+		}
 	});
-};
+}
 
-$(document).ready(main);
+	$(document).ready(main);
+
+
