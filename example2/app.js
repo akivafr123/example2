2541e16a62d359711ff45fd2bef939aa7cff62c1
Akiva Friedman-Rosner <arosner.akiva@gmail.com>
2016-03-27 16:12:18 -0400

    Make Enter Key work as expected

--- example2/app.js
+++ example2/app.js
@@ -3,15 +3,29 @@ var main = function() {
 
 	$(".comment-input button").on("click", function(event) {
 		var $new_comment;
-		
+
 		if ($(".comment-input input").val() !=="") {
 			$new_comment = $("<p>").text($(".comment-input input").val());
-			$(".comments").append($new_comment);		
+			$(".comments").append($new_comment);
+			$(".comment-input input").val("");		
 
+		}
+	});
+
+	$(".comment-input input").on("keypress", function(event) {
+		var $new_comment;
+
+		if (event.keyCode === 13) {
+			if ($(".comment-input input").val() !== "") {
+				$new_comment = $("<p>").text($(".comment-input input").val());
+				$(".comments").append($new_comment);
+				$(".comment-input input").val("");
+			}
+			
 		}
 	});
 }
 
-	$(document).ready(main);
+$(document).ready(main);
 
 
