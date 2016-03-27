bd7d57051f34a315b0fda6dec45d3b34f86a18cd
Akiva Friedman-Rosner <arosner.akiva@gmail.com>
2016-03-27 11:10:30 -0400

    make javascript fully functional

--- example2/app.js
+++ example2/app.js
@@ -2,7 +2,16 @@ var main = function() {
 	"use strict";
 
 	$(".comment-input button").on("click", function(event) {
-		$(".comments").append("<p>this is a new comment</p>");
+		var $new_comment = $("<p>"),
+			comment_text = $(".comment-input input").val();
+
+		$new_comment.text(comment_text);
+
+		// can be chained
+		// var $new_comment = $("<p>").text("this is a new comment");
+		// or
+		// var $new_comment = $("<p>").text($("comment-input input").val());
+		$(".comments").append($new_comment);
 	});
 };
 
